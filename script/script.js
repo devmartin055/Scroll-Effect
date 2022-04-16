ffcdc0b4d9e07446f6fcdcecbd6f92a5b5616fd7
CLSUTeam1Dev1 <deguzman.martin@clsu2.edu.ph>
2022-04-16 13:58:01 +0800

    made partially dynamic
    What's Done:
    - enabled configuration on translateX and translateY
    - enable configuration on the duration
    
    What's Next:
    -add play option (once or infinite)

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-cg(".child").scrollFadeIn();
+_4h('.child').scrollFadeIn();
+
+_4h(".yellow-green").scrollFadeIn(
+    {
+        translateX: "-300px",
+    }
+);
+
+_4h(".orange-red").scrollFadeIn(
+    {
+        translateX: "300px",
+    }
+);
+_4h(".mustard").scrollFadeIn({
+    duration: 3,
+    translateY: "25vh",
+});
+
--- src/scrollfadeintransition.js
+++ src/scrollfadeintransition.js
@@ -1,18 +1,27 @@
-function cg(selector) {
+function _4h(selector) {
     const self = {
         element: document.querySelectorAll(selector), //get the element DOM (document object model)
-        scrollFadeIn: () => {
+        scrollFadeIn: (settings = {}) => {
+            self.element[0].parentElement.style.overflowX = "hidden";
+            settings.translateY = typeof settings.translateY === 'undefined' ? 0 : settings.translateY;
+            settings.translateX = typeof settings.translateX === 'undefined' ? 0 : settings.translateX;
+            settings.duration = typeof settings.duration === 'undefined' ? 1 : settings.duration;
+            settings.play = typeof settings.play === 'undefined' ? "once" : settings.play;
+
+            const css = {
+                active: function (element) {
+                    return "opacity : 1; transform: translate(0, 0); transition: all " + settings.duration + "s;";
+                },
+                inactive: function (element) {
+                    return "opacity : 0; transform: translate(" + settings.translateX + ", " + settings.translateY + "); transition: all " + settings.duration + "s;";
+                }
+            }
             document.addEventListener("scroll", function (e) {
                 var pageTop = window.scrollY;
                 var pageBottom = pageTop + innerHeight;
                 for (var i = 0; i < self.element.length; i++) {
-                    if (self.element[i].offsetTop < pageBottom) {
-                        self.element[i].style.cssText =
-                            "opacity : 1; transform: translate(0, 0vh); transition: all 1s; ";
-                    } else {
-                        self.element[i].style.cssText =
-                            "opacity : 0; transform: translate(0, 10vh); transition: all 1s; ";
-                    }
+                    self.element[i].style.cssText = self.element[i].offsetTop < pageBottom ? css.active() : css.inactive();
+
                 }
             });
         },
